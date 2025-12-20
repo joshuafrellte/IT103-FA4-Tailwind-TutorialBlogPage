c02b77da17f5636446b6637369a554ebc2d56065
Joshua Frell Te <kaiserno@Joshuas-MacBook-Air.local>
2025-12-20 18:19:31 +0800

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,56 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <>
+      <header>
+        <nav className="text-white flex justify-around bg-blue-950 pt-5 pb-5 shadow-md">
+          <h1 className="font-bold">Tailwind CSS</h1>
+          <ul className='flex gap-4'>
+            <li><a href="#setup">Setup</a></li>
+            <li><a href="#utility">Utility</a></li>
+            <li><a href="#layout">Layout</a></li>
+            <li><a href="#responsive">Responsive</a></li>
+          </ul>
+        </nav>
+        <section className="text-center bg-blue-300 pt-16 pb-16">
+          <h1 className="text-5xl font-bold pt-5 pb-5">Get Started with Tailwind</h1>
+          <p>A utility-first CSS framework</p>
+          <p>Style directly in your HTML/JSX</p>
+        </section>
+      </header>
+
+      <main className="bg-blue-200">
+        <section>
+          <h2 id="setup" className="text-3xl">Setup</h2>
+          <p>First, have NodeJS installed.</p>
+        </section>
+
+        <section>
+          <h2 id="utility" className="text-3xl">Utility Classes</h2>
+          <p>One class = One style</p>
+          <p>Multiple classes componse a design</p>
+        </section>
+
+        <section>
+          <h2 id="layout" className="text-3xl">Layout</h2>
+          <h3 className="text-xl">Flex</h3>
+          <h3 className="text-xl">Grid</h3>
+        </section>
+
+        <section>
+          <h2 id="responsive" className="text-3xl">Responsive Design</h2>
+
+        </section>
+
+        <section>
+          <h2 id="" className="text-3xl">Wrapping Up</h2>
+
+        </section>
       </main>
-    </div>
+
+      <footer className="bg-blue-300 text-center pt-5 pb-5">
+        Copyright &copy; {new Date().getFullYear()} Joshua Frell Te
+      </footer>
+    </>
   );
 }
